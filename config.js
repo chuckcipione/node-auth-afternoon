48d673f6d600aa224495c54483b51f0c4fc7e5ce
Chuck <chuckcipione@yahoo.com>
2017-09-28 16:40:43 -0500

    node-auth-afternoon

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    domain: 'gnarls.auth0.com',
+    clientID: '0O7ZNm6HLdxvPbq42QacOxuGFCeoNPwS',
+    clientSecret: 'TcN_iqmANjxjOqu19a7-3jkVyyi0NDkSA8RVxaxQGrD2oTylU2Sxq6nmc0fwx5ds'
+}
